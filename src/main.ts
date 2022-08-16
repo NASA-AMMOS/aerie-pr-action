c721db8238b60a0a59828e6640a62b1546dabf46
Luke Jurgella <skovati@protonmail.com>
2022-08-16 11:03:54 -0700

    Remove custom reviewers logic

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
-import fs from "fs";
 
 async function run(): Promise<void> {
     try {
@@ -12,8 +11,6 @@ async function run(): Promise<void> {
             throw new Error("Error, not triggered from PR, aborting...");
         }
 
-        // parse inputs
-        const n: number = +core.getInput("numReviewers", { required: true });
         const token: string = core.getInput("token", { required: true });
 
         // create auth'd github api client
@@ -53,38 +50,6 @@ async function run(): Promise<void> {
             `resp: ${status}, assigned ${assignees} to PR ${pull_number} in ${repo}`
         );
 
-        // assign n reviewers randomly from REVIEWERS
-        core.info("Detecting reviewers...");
-        const reviewer_raw = fs.readFileSync("./.github/REVIEWERS", "utf8");
-        const reviewers = reviewer_raw
-            .trimEnd()
-            .split(" ")
-            .filter(v => v !== user.login); // don't allow PR opener to be reviewer
-        if (reviewers.length < n) {
-            throw new Error(
-                "Error, supplied n is greater than length of reviewers, can't assign reviewers"
-            );
-        }
-        core.info("Found: ");
-        for (const c of reviewers) {
-            core.info(c);
-        }
-
-        // shuffle list and take first n elemenets
-        const to_review = reviewers
-            .sort(() => 0.5 - Math.random()) // ¯\_(ツ)_/¯
-            .slice(0, n);
-        core.info("Assigning the following as reviwers...");
-        for (const c of to_review) {
-            core.info(c);
-        }
-
-        gh.rest.pulls.requestReviewers({
-            owner,
-            repo,
-            pull_number,
-            reviewers: to_review
-        });
     } catch (error) {
         if (error instanceof Error) core.setFailed(error.message);
     }
