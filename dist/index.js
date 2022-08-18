cc1e9e770e0d4c89edc587acd307c17290f5f3c3
Luke Jurgella <skovati@protonmail.com>
2022-08-18 13:32:38 -0700

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -41,7 +41,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(2186));
 const github = __importStar(__nccwpck_require__(5438));
-const triggers = ["labeled", "unlabeled", "synchronize", "submitted", "edited", "dismissed"];
+const triggers = [
+    "labeled",
+    "unlabeled",
+    "synchronize",
+    "submitted",
+    "edited",
+    "dismissed"
+];
 let gh;
 function run() {
     var _a;
